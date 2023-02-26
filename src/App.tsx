7cd2ff68e7659f1ab4fba1dd59ac1e34360a74ad
Hugo Moreira <hugo.n4@gmail.com>
2023-02-25 21:58:00 -0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import { useState } from 'react'
+
+import Landing from './pages/Landing'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Landing />
     </div>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,31 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import Landing from './pages/Landing';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Landing />,
+  },
+  {
+    path: "/app",
+  },
+  {
+    path: "/orphanages/create",
+  },
+  {
+    path: "/orphanages/:id",
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
